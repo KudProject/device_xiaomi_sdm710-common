f7cc5afd421d43525b850021f74a25789ab3119e
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-18 09:09:17 -0300

    sdm710-common: overlay: Ignore RSSNR signal level
    Improve reception
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- sdm710.mk
+++ sdm710.mk
@@ -14,7 +14,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm710-common/sdm710-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-xenonhd
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
