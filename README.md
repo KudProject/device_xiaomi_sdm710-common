e1c48136ff3d378e84808de74dc28a0ebd2b2d55
Albert I <kras@raphielgang.org>
2019-07-13 21:59:48 +0800

    sdm710-common: Xenon-ify
    Signed-off-by: Albert I <kras@raphielgang.org>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Copyright (C) 2018 - The LineageOS Project
+Copyright (C) 2018-2019 - The LineageOS Project
+Copyright (C) 2019 - The XenonHD Project
 
 Common device tree for Xiaomi SDM710 based devices
 ==============
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2019 The XenonHD Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,9 +14,9 @@ INITIAL_COPYRIGHT_YEAR=2018
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+XENONHD_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${XENONHD_ROOT}/vendor/xenonhd/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,7 +24,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${XENONHD_ROOT}" true
 
 # Copyright headers and guards
 write_headers "grus sirius"
@@ -37,7 +38,7 @@ write_footers
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${XENONHD_ROOT}" false
 
     # Copyright headers and guards
     write_headers
