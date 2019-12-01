10ffd6d872b918bd5996aa665d145804329157b4
Albert I <kras@raphielgang.org>
2019-12-01 15:35:41 +0800

    sdm710-common: Welcome (back) to LineageOS
    * Remove changes specific to XenonHD out from device tree
    
    Change-Id: I02271a27440064adec82dbae2947485c9d9717b7
    Signed-off-by: Albert I <kras@raphielgang.org>

--- sdm710.mk
+++ sdm710.mk
@@ -14,8 +14,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm710-common/sdm710-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-xenonhd
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
