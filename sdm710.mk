1e048bc48a1bd0596b4fb619a280205f4bc93e69
Albert I <kras@raphielgang.org>
2021-06-03 00:29:29 +0800

    sdm710-common: Bring back LiveDisplay
    Standard features for now. SunlightEnhancement remains unavailable until
    relevant kernel features are re-introduced and the service invocation is
    reworked.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- sdm710.mk
+++ sdm710.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm710
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
