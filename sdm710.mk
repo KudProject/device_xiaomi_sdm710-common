1b5b5da5f654e50edd27bb9cf23c17da0017331b
Albert I <kras@raphielgang.org>
2021-06-02 23:40:03 +0800

    sdm710-common: Don't ship Codec2 media profiles if building with GMS
    TODO: This should be done only when we're not shipping Google APEX modules.
    
    These profiles are already shipped as part of com.google.android.media.swcodec.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- sdm710.mk
+++ sdm710.mk
@@ -121,10 +121,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Codec2
+ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+endif
 
 # Common init scripts
 PRODUCT_PACKAGES += \
